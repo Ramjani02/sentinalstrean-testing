9bc2daa62e03e4ae8cf18fc2b46e89e354606542
vikramraj1022 <94282799+vikramraj1022@users.noreply.github.com>
2026-03-06 17:35:35 +0530

    Set up asynchronous database engine and session

--- app/db.py
+++ app/db.py
@@ -1,1 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from app.config import DATABASE_URL
+from sqlalchemy.orm import declarative_base
 
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+Base = declarative_base()

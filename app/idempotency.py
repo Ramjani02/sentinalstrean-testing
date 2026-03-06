301617a45fb4c31188d04009a1a63ad34cdba3e9
vikramraj1022 <94282799+vikramraj1022@users.noreply.github.com>
2026-03-06 17:37:51 +0530

    Add Redis functionality for key management

--- app/idempotency.py
+++ app/idempotency.py
@@ -1,1 +1,10 @@
+import redis
+from app.config import REDIS_URL
 
+r = redis.Redis.from_url(REDIS_URL)
+
+def check_key(key: str):
+    return r.get(key)
+
+def store_key(key: str, value: str):
+    r.setex(key, 86400, value)
